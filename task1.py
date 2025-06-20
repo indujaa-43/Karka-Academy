dd727238652863b6821427c6ee8ebdc98f3a13d5
indujaa-43 <indujaarajesh@gmail.com>
2025-06-20 09:35:04 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,52 @@
+#print("Hello, world!")
+#print("Indujaa R S")
+#print("Indujaa R.S")
+#print(22)
+#name="Indujaa R.S"
+#age=22
+#height=5.4
+#is_student=True
+#print("Name:",name)
+#print("Age+1:",age+1)
+#print("Height in cm:",height*30.48)
+#print("Is student?", is_student)
+#name="Indujaa R.S"
+#age=22
+#print("My name is"+ name+" and  I am" +str (age)+ "years old.")
+#my_name="Indujaa R.S"
+#my_age=22
+#print("Name:", my_name)
+#print("Age:",my_age)
+#a=10
+#print("Subraction:", a-b)
+#print("Division:", a/b)   
+#for i in range(1,6):
+ #   print("*" * i)
+#ame="Indujaa R.S"
+#age=22
+#print("Name:", name)
+#print("Is Student:", isstudent)
+#
+#name="ashiha"
+#age+23
+#isstudent = True
+#print("Modified values")
+#print("Nmae:", name)
+#print("Age:", age)
+#print("Is student:", isstudent)
+name="Indujaa R.S"
+age=22
+dob=27/1/2005
+height=5.4
+weight=55
+degree="B.Tech Artificial Intelligence and Data Science"
+gender="Female"
+print("Personal Details:")
+print("Name:", name)
+print("Age:", age)
+print("Date of Birth:", dob)
+print("Height:", height)
+print("Weight:", weight)
+print("Degree:", degree)
+print("Gender:", gender)
+   
