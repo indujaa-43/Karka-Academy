4a770d92fa28c0914867b4f19b431206771b6f75
indujaa-43 <indujaarajesh@gmail.com>
2025-06-21 20:34:04 +0530

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,59 @@
+#task 2
+#def add(a, b):
+ #  return a + b
+#print("Sum 1:", result1)
+#result2 = add(-3, 7)
+#print("Sum 2:", result2)
+#result3 = add(100, 250)
+#print("Sum 3:", result3)
+
+#task 1
+#def sayHello():
+ #   print("Hello, World!")
+#sayHello()
+#def sayHello(name):
+ #   print(f"Hello, {name}!")
+#sayHello("Alice")
+#sayHello("Indujaa")
+
+#task 3
+#multiply = lambda x, y: x * y
+#print("multiply(5, 3):", multiply(5, 3))
+#print("multiply(10, -2):", multiply(10, -2))
+#print("multiply(0, 100):", multiply(0, 100))
+
+#task 4
+#def multiply(a, b):
+ #   return a * b
+#print("multiply(4, 7):", multiply(4, 7))
+#print("multiply(-1, 8):", multiply(-1, 8))
+#print("multiply(6, 0):", multiply(6, 0))
+
+#task 5
+#def divide(a, b):
+ #   if b != 0:
+  #      return a / b
+   # else:
+   #     return "Cannot divide by zero"
+#print("divide(10, 2):", divide(10, 2))
+#print("divide(7, 0):", divide(7, 0))
+#print("divide(20, 4):", divide(20, 4))
+
+#task 6
+#def factorial(n):
+ #   if n < 0:
+ #       return "Factorial not defined for negative numbers"
+ #    result = 1
+ #      for i in range(1, n + 1):
+#       result *= i
+#     return result
+#print("factorial(5):", factorial(5))
+#print("factorial(0):", factorial(0))
+#print("factorial(3):", factorial(3))
+
+#task 7
+def square(number):
+    return number ** 2
+print("square(4):", square(4))
+print("square(-3):", square(-3))
+print("square(0):", square(0))
